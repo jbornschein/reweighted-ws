bd2cb5403a0fbcbedb4bfb65673ea855d92ce039
Jorg Bornschein <jb@capsec.org>
2014-05-13 05:25:20 +0200

    Change lookahead in EarlyStopping

--- learning/termination.py
+++ learning/termination.py
@@ -29,27 +29,27 @@ class Termination(object):
 
 #-----------------------------------------------------------------------------
 class LogLikelihoodIncrease(Termination):
-    def __init__(self, min_increase=0.001, interval=5, max_epochs=1000, min_epochs=10):
+    def __init__(self, min_increase=0.001, lookahead=5, max_epochs=1000, min_epochs=10):
         super(LogLikelihoodIncrease, self).__init__()
         
         self.min_epochs = min_epochs
         self.max_epochs = max_epochs
-        self.interval = interval
+        self.lookahead = lookahead
         self.min_increase = min_increase
 
         self.reset()
         
     def reset(self):
         self.epochs = 0
-        self.L = [-np.inf]*self.interval
+        self.L = [-np.inf]*self.lookahead
 
     def continue_learning(self, L):
         self.epochs += 1
         self.L.append(L)
 
-        best_in_interval     = np.array(self.L[-self.interval:]).max()
-        best_before_interval = np.array(self.L[:-self.interval]).max()
-        increase = (best_in_interval-best_before_interval)/(np.abs(best_before_interval))
+        best_in_lookahead     = np.array(self.L[-self.lookahead:]).max()
+        best_before_lookahead = np.array(self.L[:-self.lookahead]).max()
+        increase = (best_in_lookahead-best_before_lookahead)/(np.abs(best_before_lookahead))
 
         if np.isnan(increase):
             increase = +np.inf
@@ -62,12 +62,13 @@ class LogLikelihoodIncrease(Termination):
 
 #-----------------------------------------------------------------------------
 class EarlyStopping(Termination):
-    def __init__(self, interval=5, max_epochs=1000):
+    def __init__(self, lookahead=10, min_epochas=10, max_epochs=1000):
         super(EarlyStopping, self).__init__()
         
+        self.lookahead = lookahead
+        self.min_epochs = min_epochs
         self.max_epochs = max_epochs
         self.epochs = 0
-        self.interval = interval
         self.fails = 0
         self.best_LL = -np.inf
 
@@ -81,6 +82,9 @@ class EarlyStopping(Termination):
         L = monitor.validation_LL
         assert isinstance(L, float)
 
+        if self.epochs <= self.min_epochs:
+            return True
+
         if self.epochs > self.max_epochs:
             return False
 
@@ -93,5 +97,5 @@ class EarlyStopping(Termination):
             self.fails += 1
             _logger.info("Validation LL=%5.2f stagnated (%dth)" % (L, self.fails))
 
-        return self.fails < self.interval
+        return self.fails < self.lookahead
 
