6dec4bd2f2c15b85f3a4ec23daf0701589ff995c
Jorg Bornschein <jb@capsec.org>
2014-06-15 17:29:39 +0200

    Add download.sh script

---
+++ data/download.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+
+DOWN=wget
+#DOWN=curl
+
+$DOWN http://www.capsec.org/datasets/adult.h5
+$DOWN http://www.capsec.org/datasets/connect4.h5
+$DOWN http://www.capsec.org/datasets/connect4.h5
+$DOWN http://www.capsec.org/datasets/dna.h5
+$DOWN http://www.capsec.org/datasets/mnist.pkl.gz
+$DOWN http://www.capsec.org/datasets/mnist_salakhutdinov.pkl.gz
+$DOWN http://www.capsec.org/datasets/mushrooms.h5
+$DOWN http://www.capsec.org/datasets/ocr_letters.h5
+$DOWN http://www.capsec.org/datasets/rcv1.h5
+$DOWN http://www.capsec.org/datasets/web.h5
+
