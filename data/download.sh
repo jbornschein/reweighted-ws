cde1bda8292625738f989b61e18c46aa027ca04f
Jorg Bornschein <jb@capsec.org>
2014-10-21 13:11:56 -0400

    Fix typo in dataset-download script

--- data/download.sh
+++ data/download.sh
@@ -6,7 +6,6 @@ DOWN=wget
 
 $DOWN http://www.capsec.org/datasets/adult.h5
 $DOWN http://www.capsec.org/datasets/connect4.h5
-$DOWN http://www.capsec.org/datasets/connect4.h5
 $DOWN http://www.capsec.org/datasets/dna.h5
 $DOWN http://www.capsec.org/datasets/mnist.pkl.gz
 $DOWN http://www.capsec.org/datasets/mnist_salakhutdinov.pkl.gz
